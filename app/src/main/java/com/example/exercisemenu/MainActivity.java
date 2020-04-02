2c399999041611f2ddaa78d6f9904ddf7a6fc2a3
adiyogta <adiyogta@gmail.com>
2020-04-02 18:54:48 +0700

    1

--- app/src/main/java/com/example/exercisemenu/MainActivity.java
+++ app/src/main/java/com/example/exercisemenu/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.exercisemenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button signin;
+    EditText un,ps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signin = (Button) findViewById(R.id.btnSignin);
+
+
     }
 }
