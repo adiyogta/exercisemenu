b17fe4513975b5c5d095d203decd61bf08f9f523
adiyogta <adiyogta@gmail.com>
2020-04-03 01:07:34 +0700

    3 menu hitung

--- app/src/main/java/com/example/exercisemenu/menu.java
+++ app/src/main/java/com/example/exercisemenu/menu.java
@@ -30,19 +30,88 @@ public class menu extends AppCompatActivity {
                 if(spinner1.getSelectedItem().toString().equals("Hitung Luas")) {
                     Intent i = new Intent(menu.this, luasPersegi.class);
                     startActivity(i);
-                } else if (spinner1.getSelectedItem().toString().equals("Hitung Luas")){
+                } else if (spinner1.getSelectedItem().toString().equals("Hitung Keliling")){
                     Intent i = new Intent(menu.this, kelilingPersegi.class);
                     startActivity(i);
                 } else {
-
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
 
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(spinner2.getSelectedItem().toString().equals("Hitung Luas")) {
+                    Intent i = new Intent(menu.this, luasLingkaran.class);
+                    startActivity(i);
+                } else if (spinner2.getSelectedItem().toString().equals("Hitung Keliling")){
+                    Intent i = new Intent(menu.this, kelilingLingkaran.class);
+                    startActivity(i);
+                } else {
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(spinner3.getSelectedItem().toString().equals("Hitung Luas")) {
+                    Intent i = new Intent(menu.this, luasPersegiP.class);
+                    startActivity(i);
+                } else if (spinner3.getSelectedItem().toString().equals("Hitung Keliling")){
+                    Intent i = new Intent(menu.this, KelilingPersegiP.class);
+                    startActivity(i);
+                } else {
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        spinner4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(spinner4.getSelectedItem().toString().equals("Hitung Luas")) {
+                    Intent i = new Intent(menu.this, luasSegitiga.class);
+                    startActivity(i);
+                } else if (spinner4.getSelectedItem().toString().equals("Hitung Keliling")){
+                    Intent i = new Intent(menu.this, kelilingSegitiga.class);
+                    startActivity(i);
+                } else {
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        spinner5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(spinner5.getSelectedItem().toString().equals("Hitung Luas")) {
+                    Intent i = new Intent(menu.this, luasTrapesium.class);
+                    startActivity(i);
+                } else if (spinner5.getSelectedItem().toString().equals("Hitung Keliling")){
+                    Intent i = new Intent(menu.this, kelilingTrapesium.class);
+                    startActivity(i);
+                } else {
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
 }
