b76b4f111694f5af82d6d55386ad6c1f1997a96d
adiyogta <adiyogta@gmail.com>
2020-04-02 22:15:11 +0700

    dropdown

--- app/src/main/java/com/example/exercisemenu/menu.java
+++ app/src/main/java/com/example/exercisemenu/menu.java
@@ -2,13 +2,47 @@ package com.example.exercisemenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class menu extends AppCompatActivity {
+    Spinner spinner1, spinner2, spinner3, spinner4, spinner5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        spinner3 = (Spinner) findViewById(R.id.spinner3);
+        spinner4 = (Spinner) findViewById(R.id.spinner4);
+        spinner5 = (Spinner) findViewById(R.id.spinner5);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(spinner1.getSelectedItem().toString().equals("Hitung Luas")) {
+
+                    Intent i = new Intent(menu.this, luasPersegi.class);
+                    startActivity(i);
+
+                } else {
+                    Intent i = new Intent(menu.this, kelilingPersegi.class);
+                    startActivity(i);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
